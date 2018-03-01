286896b4518d4f80a0321e72ed61443e5266718a
dimakorzhovnik <korzhovnik.dima@gmail.com>
2018-03-01 22:04:13 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function longestConsecutiveLength(array) {
   // your solution here
+    // your solution here
+    array.sort((a,b)=>a-b);
+    console.log(array);
+    var maxSeq = 0, tempSeq = 0;
+    if(array.length == 0){
+      return 0;
+    }
+    for(let i = 0; i < array.length-1; i++){
+        var resultVal = array[i+1]-array[i];
+        if(resultVal==1){
+            tempSeq++;
+        }else if(resultVal!=0){
+            if(tempSeq > maxSeq){
+                maxSeq = tempSeq;
+                tempSeq = 0;
+            }
+            tempSeq = 0;
+        }
+    }
+    if(maxSeq ==0){
+      
+        maxSeq = tempSeq;
+    }
+  return maxSeq+1;
 }
